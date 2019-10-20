1b69b4f8bc11c1cf0d50a9f5ae0264f242a047e3
vermad843 <vishnuverma7524@gmail.com>
2019-10-20 10:44:56 +0530

    getting apple randomly

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,17 @@ import React, { Component } from 'react';
 import Snake from './Snake';
 import Food from './Food';
 
+const getRandomCoordinates = () => {
+  let min = 1;
+  let max = 98;
+  let x = Math.floor((Math.random()*(max-min+1)+min)/2)*2;
+  let y = Math.floor((Math.random()*(max-min+1)+min)/2)*2;
+  return [x,y]
+}
+
 class App extends Component {
    state = {
-      food : [6,8],
+      food : getRandomCoordinates(),
      snakeDots : [
        [0,0],
        [2,0]
