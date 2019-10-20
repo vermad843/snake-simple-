cb9c5bae5a92d6daa0c3d044a9d2972bd1c34cbe
vermad843 <vishnuverma7524@gmail.com>
2019-10-20 09:51:47 +0530

    adding snake to the page

--- src/App.js
+++ src/App.js
@@ -3,9 +3,12 @@ import React, { Component } from 'react';
 class App extends Component {
   render() {
     return (
-      <div className = "game-area" >
-
+      <div className = "game-area">
+         <div className = "snake-dot" style = {{top:0, left:0}}></div>
+         <div className = "snake-dot" style = {{top:0, left:'2%'}}></div>
+         <div className = "snake-dot" style = {{top:0, left:'4%'}}></div>
       </div>
+      
     );
   }
 }
